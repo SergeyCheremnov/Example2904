589c586bf1be4497fee771566af78f01d418beba
SergeyCheremnov <Scheremnov-serge@mail.ru>
2022-04-28 21:07:38 +0600

    ПРИВЕТ МАША

---
+++ Example006_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя:   ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("УРА ЭТОЖЕ МАША");
+}
+else
+{
+    Console.Write("Привет,   ");
+    Console.WriteLine(username);
+}
